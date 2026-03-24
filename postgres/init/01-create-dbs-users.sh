385451e6958b53fd724e6885d133639da3d79abf
K0taka <144228630+K0taka@users.noreply.github.com>
2026-03-24 16:56:28 +0500

    comments

--- postgres/init/01-create-dbs-users.sh
+++ postgres/init/01-create-dbs-users.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 : "${STUDY_DB_PASSWORD:?STUDY_DB_PASSWORD is required}"
 
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
-  -- 1) Roles/users (idempotent)
+  -- Roles/users
   DO \$\$
   BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${CORE_DB_USER}') THEN
@@ -22,14 +22,15 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
   END
   \$\$;
 
-  -- 2) Databases (idempotent) - cannot be inside DO block
+  -- Databases
   SELECT format('CREATE DATABASE %I OWNER %I', '${CORE_DB}', '${CORE_DB_USER}')
   WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${CORE_DB}') \gexec
 
   SELECT format('CREATE DATABASE %I OWNER %I', '${STUDY_DB}', '${STUDY_DB_USER}')
   WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '${STUDY_DB}') \gexec
 
-  -- 3) Ensure connect privileges (safe even if already granted)
+  -- Ensure connect privileges
   GRANT CONNECT ON DATABASE ${CORE_DB} TO ${CORE_DB_USER};
   GRANT CONNECT ON DATABASE ${STUDY_DB} TO ${STUDY_DB_USER};
 EOSQL
+
--- postgres/init/02-grants.sh
+++ postgres/init/02-grants.sh
@@ -6,17 +6,17 @@ set -euo pipefail
 : "${CORE_DB_USER:?CORE_DB_USER is required}"
 : "${STUDY_DB_USER:?STUDY_DB_USER is required}"
 
-# 1) Allow users to connect to their databases
+# Allow users to connect to their databases
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-EOSQL
   GRANT CONNECT ON DATABASE ${CORE_DB} TO ${CORE_DB_USER};
   GRANT CONNECT ON DATABASE ${STUDY_DB} TO ${STUDY_DB_USER};
 EOSQL
 
-# 2) Grants inside core_db
+# Grants inside core_db
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "${CORE_DB}" <<-EOSQL
   GRANT USAGE, CREATE ON SCHEMA public TO ${CORE_DB_USER};
 
-  -- EF Core migrations create tables/sequences; default privileges help future objects
+  -- EF Core migrations create tables/sequences;
   ALTER DEFAULT PRIVILEGES IN SCHEMA public
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${CORE_DB_USER};
 
@@ -24,7 +24,7 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "${CORE_DB}" <<-EOSQL
     GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO ${CORE_DB_USER};
 EOSQL
 
-# 3) Grants inside study_db
+# Grants inside study_db
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "${STUDY_DB}" <<-EOSQL
   GRANT USAGE, CREATE ON SCHEMA public TO ${STUDY_DB_USER};
 
